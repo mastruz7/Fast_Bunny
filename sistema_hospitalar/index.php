b358094342581f9675525dc88d9b805765c00ec5
cahjustino <66473182+cahjustino@users.noreply.github.com>
2023-05-15 18:36:59 -0300

    Add files via upload
    inicialização do código frontend e backend

---
+++ sistema_hospitalar/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>G Hospital</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    </head>
+    <body>
+        <?php
+     include("include/header.php");
+
+    ?>
+
+    <div style="margin-top:50px"></div>
+
+    <div class="container">]
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3 mx-1 shadow">
+                    // foto informçao
+                    <img src="img/informacao.jpg" style="width: 100%">
+                    <h5 class="text-center">Mais informaçoes</h5>
+
+                    <a href="">
+                        <button class="bnt bnt-sucess" style="margin-left:30%" >clique aqui</button>
+                    </a>
+
+                </div>
+                <div class="col-md-3 mx-1 shadow">
+                    //foto pasciente
+                    <img src="img/cuidados.jpg" style="width: 100%">
+                    <h5 class="text-center">Para se cadastrar</h5>
+
+                    <a href="">
+                        <button class="bnt bnt-sucess" style="margin-left:30%" >cadastre-se</button>
+                    </a>
+
+                </div>
+                <div class="col-md-3 mx-1 shadow">
+                    //foto medico
+                    <img src="img/equipes.jpg" style="width: 100%">
+                    <h5 class="text-center">Trabalhe conosco</h5>
+
+                    <a href="">
+                        <button class="bnt bnt-sucess" style="margin-left:30%" >clique aqui</button>
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+        
+   
+    </body>
+</html>
