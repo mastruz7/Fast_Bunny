405e654202ad8140c6978e4fee7e1efe2807e522
cahjustino <66473182+cahjustino@users.noreply.github.com>
2023-05-15 18:40:44 -0300

    Add files via upload

---
+++ sistema_hospitalar/include/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        
+        <!-- Bootstrap CSS-->
+
+        <link rel="stylesheet"  type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/
+        4.5.0/css/bootstrap.min.css">
+
+        <!-- Bootstrap JS -->
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="
+        sha256-DrT5NfxfbHvMHux31LKhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <link rel="stylesheet"  type = "text/css" href="https://stackppath.bootstrapcdn.com/
+        font-awesome/4.7.0/css/font-awesome.min.css" >
+    </head>
+    <body>
+        <!-- navibar -->
+        <nav class="navbar navbar-expand-lg navbar-info" bg-info>
+            <h5 class="text-white">Systema de Gerenciamento Hospitalar</h5>
+
+            <div class="mr-auto"></div>
+
+            <ul class="navbar-nav" >
+                <li class="nav-item"><a href="/login.php" class="nav-item">Administração</a></li>
+                <li class="nav-item"><a href="" class="nav-item">Médico</a></li>
+                <li class="nav-item"><a href="" class="nav-item">Paciente</a></li>
+                
+            </ul>
+
+        </nav>
+
+    </body>
+</html>
---
+++ sistema_hospitalar/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<htm>
+    <head>
+        <title>Pagina de Login</title>
+
+    </head>
+    <body>
+        <?php
+        include("inclide/header.php");
+        ?>
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-3"> </div>
+                    <div class="col-md-6">
+                        <form method="post">
+                        
+                        </form>
+                    </div>
+                    <div class="col-md-3"> </div>
+
+                </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </body>
+</htm>
